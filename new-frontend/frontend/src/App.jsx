78f52a6cf449c3ed625080c126bdce53bc5d13d1
MAYRISHYA <pmayrishya@gmail.com>
2026-04-24 20:11:06 +1000

    Added navbar, footer, and improved homepage layout 2

--- new-frontend/frontend/src/App.jsx
+++ new-frontend/frontend/src/App.jsx
@@ -1,36 +1,22 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'chartjs-adapter-date-fns';
-import HomePage from './pages/HomePage';
-import DashboardPage from './pages/DashboardPage';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "chartjs-adapter-date-fns";
 
-// import DashboardLearn from './components/DashboardLearn'; //for learning 
-
-import FetchData from './components/FetchData';
-
-
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import FetchData from "./components/FetchData";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1>IoT Sensors Dashboard</h1>
-        <h3>Time-series Sensor Data and Correlation Analysis</h3>
-        <Routes>
-          
-          
-          <Route path="/sensorData1" element={<FetchData />} /> 
-          <Route path="/" element={<HomePage />} /> 
-          <Route path="/dashboard/:id" element={<DashboardPage />} />
-
-                  
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/sensorData1" element={<FetchData />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard/:id" element={<DashboardPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
-
 
 export default App;
