205adeb7c7738d33b89f9d7e2b684cd9a72dc957
JoshStacky <jdstacky1@gmail.com>
2026-04-30 17:07:48 +1000

    Update pipeline.py

--- data_science/pipeline.py
+++ data_science/pipeline.py
@@ -1,21 +1,25 @@
 import sys
 from preprocessor import load_and_prepare
+from detectors.adtk_pcaad import PcaADDetector
+from detectors.ocsvm_detector import OCSVMDetector
+from detectors.levelshiftad import LevelShiftAD
 
 def run_pipeline(filepath):
     print(f"[pipeline] Loading data from: {filepath}")
     df, scaler = load_and_prepare(filepath)
-
     print(f"[pipeline] Shape following preprocessor acting: {df.shape}")
     print(f"[pipeline] Columns {list(df.columns)}")
     print(f"[pipeline] preview/check\n{df.head()}\n")
 
-    #  DETECTOR
-    from detectors.adtk_pcaad import PcaADDetector
-    detectors = [PcaADDetector()]
-    # Link the detectors we implement below so others can draw on them if need be.
+    # DETECTOR
+    detectors = [
+        PcaADDetector(),
+        OCSVMDetector(nu=0.05),
+        LevelShiftAD(),
+    ]
+ # Link the detectors we implement below so others can draw on them if need be.
     # if theres any requirements for your detector maybe note it here as well.
     
-
     results = {}
     for detector in detectors:
         name = type(detector).__name__
@@ -26,7 +30,6 @@ def run_pipeline(filepath):
         print("[pipeline] preprocessing works")
 
     return df, scaler, results
-
 
 if __name__ == "__main__":
     filepath = sys.argv[1] if len(sys.argv) > 1 else "datasets/complex.csv"
