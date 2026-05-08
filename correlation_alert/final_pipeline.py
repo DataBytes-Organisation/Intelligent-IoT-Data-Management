d12efa47c0b934c3025443d7dc7646146eff5ad4
Senura Perera <senuradp@gmail.com>
2026-05-08 20:23:28 +1000

    Validate integrated pipeline using real IoT dataset

--- correlation_alert/final_pipeline.py
+++ correlation_alert/final_pipeline.py
@@ -1,17 +1,20 @@
 import pandas as pd
 from main import detect_correlation_change_alert
 
-df = pd.DataFrame({
-    "timestamp": pd.date_range(start="2026-01-01", periods=100, freq="min"),
-    "sensor_1": list(range(100)),
-    "sensor_2": list(range(0, 200, 2)),
-    "sensor_3": list(range(50)) + list(range(50, 0, -1))
-})
+# df = pd.DataFrame({
+#     "timestamp": pd.date_range(start="2026-01-01", periods=100, freq="min"),
+#     "sensor_1": list(range(100)),
+#     "sensor_2": list(range(0, 200, 2)),
+#     "sensor_3": list(range(50)) + list(range(50, 0, -1))
+# })
+# df = pd.read_csv("datasets/2881821.csv")
+df = pd.read_csv("../datasets/complex.csv")
+df.columns = df.columns.str.strip()
 
 result = detect_correlation_change_alert(
     df=df,
-    timestamp_col="timestamp",
-    selected_streams=["sensor_1", "sensor_2", "sensor_3"],
+    timestamp_col="time",
+    selected_streams=["s1", "s2", "s3"],
     window_size=20,
     step_size=10
 )
